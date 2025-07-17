843c4fcfda2e0ca4ee55f46418980b48d7fffc87
melissa <132623109+melyho@users.noreply.github.com>
2025-07-17 10:33:37 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/schedule',
-        destination: 'https://docs.google.com/spreadsheets/d/1Rfwuv77u6AXpzAlL0U4pgK-d0Jkx63zf2MfhcPHrAJA/edit?gid=1048327872#gid=1048327872', // Replace with your target URL
-        permanent: true, // true = 301 redirect, false = 302
+        //source: '/schedule',
+        //destination: 'https://docs.google.com/spreadsheets/d/1Rfwuv77u6AXpzAlL0U4pgK-d0Jkx63zf2MfhcPHrAJA/edit?gid=1048327872#gid=1048327872', // Replace with your target URL
+        //permanent: true, // true = 301 redirect, false = 302
       },
     ];
   },
