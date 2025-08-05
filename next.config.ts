0fbd83bf6ceb8e9edd50834887fd604d99282619
melissa <132623109+melyho@users.noreply.github.com>
2025-08-04 21:35:38 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  //async redirects() {
-    //return [
-     // {
-        //source: '/schedule',
-        //destination: 'https://docs.google.com/spreadsheets/d/1Rfwuv77u6AXpzAlL0U4pgK-d0Jkx63zf2MfhcPHrAJA/edit?gid=1048327872#gid=1048327872', // Replace with your target URL
-        //permanent: true, // true = 301 redirect, false = 302
-    //  },
-  //  ];
-//  },
+  async redirects() {
+    return [
+     {
+        source: '/schedule',
+        destination: 'https://docs.google.com/spreadsheets/d/1Rfwuv77u6AXpzAlL0U4pgK-d0Jkx63zf2MfhcPHrAJA/edit?usp=sharing',
+        permanent: true, // true = 301 redirect, false = 302
+  },
+   ];
+  },
 };
 
 export default nextConfig;
